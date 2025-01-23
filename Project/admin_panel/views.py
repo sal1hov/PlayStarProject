2e024ef4430fb27b20336f606e6b5348fa57270f
sal1hov <nhtxwd@gmail.com>
2025-01-23 11:21:03 +0300

    Commit

--- Project/admin_panel/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponseForbidden
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def admin_dashboard(request):
-    if request.user.role == 'admin':
-        return render(request, 'admin_panel/admin_dashboard.html')
-    return HttpResponseForbidden("Доступ запрещен")
--- Project/bookings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('client/', views.client_dashboard, name='client_dashboard'),
-    path('employee/', views.employee_dashboard, name='employee_dashboard'),
-    path('manager/', views.manager_dashboard, name='manager_dashboard'),
-    path('admin/', views.admin_dashboard, name='admin_dashboard'),
-]
