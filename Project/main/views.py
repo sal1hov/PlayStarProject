42ec861ee39340f29a2fa74306426781f33b30bc
sal1hov <nhtxwd@gmail.com>
2025-01-24 13:50:00 +0300

    Изменение настроек формы регистрации

--- Project/main/views.py
+++ Project/main/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-
+from .forms import RegisterForm  # Импортируем кастомную форму для регистрации
 
 def index(request):
     return render(request, 'main/index.html')
-
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)  # Используем кастомную форму
         if form.is_valid():
             user = form.save()
             login(request, user)  # Автоматический вход после регистрации
             return redirect('index')  # Перенаправление на главную страницу
     else:
-        form = UserCreationForm()
+        form = RegisterForm()  # Используем кастомную форму
     return render(request, 'registration/register.html', {'form': form})
 
 
