5f465e7d66f2c8b0393f85dfc4d9348f4b085597
sal1hov <nhtxwd@gmail.com>
2025-01-26 02:21:59 +0300

    Изменение

---
+++ Project/accounts/static/accounts/js/scripts.js
@@ -0,0 +1,9 @@
+// Скрипт для управления модальным окном в шаблоне profile_edit.html
+    function openModal() {
+        document.getElementById('modal').classList.remove('hidden');
+    }
+
+    function closeModal() {
+        document.getElementById('modal').classList.add('hidden');
+    }
+
