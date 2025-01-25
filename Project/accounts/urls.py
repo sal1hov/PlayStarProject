cd2ebaa3c7a782e1d6c82e0dabe3f059470c4eff
sal1hov <nhtxwd@gmail.com>
2025-01-25 12:28:43 +0300

    Обновление файла accounts/urls.py

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import profile, profile_edit
+from .views import profile, profile_edit, delete_child
 
 urlpatterns = [
     path('profile/', profile, name='profile'),
     path('profile/edit/', profile_edit, name='profile_edit'),
+    path('profile/delete_child/<int:child_id>/', delete_child, name='delete_child'),
 ]
