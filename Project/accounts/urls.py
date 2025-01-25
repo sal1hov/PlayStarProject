a47d65e6c764d02ab1e449c90eb5e239e0625c8e
sal1hov <nhtxwd@gmail.com>
2025-01-25 03:12:28 +0300

    Изменение путей в aссounts.py

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profile
+from .views import profile, profile_edit
 
 urlpatterns = [
     path('profile/', profile, name='profile'),
+    path('profile/edit/', profile_edit, name='profile_edit'),
 ]
