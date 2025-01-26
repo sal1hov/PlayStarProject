d51bd320faafd1165bcafd9e5a1e3a30cbf0d7a7
sal1hov <nhtxwd@gmail.com>
2025-01-26 04:39:34 +0300

    Обновление settings.py

--- Project/core/settings.py
+++ Project/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'core.urls'
 
@@ -122,8 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Папка, куда будут собраны статические файлы
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Убедитесь, что путь указан правильно
+    os.path.join(BASE_DIR, 'static'),  # Папка с вашими статическими файлами в разработке
 ]
 
 # Default primary key field type
