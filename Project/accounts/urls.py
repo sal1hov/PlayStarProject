c8d1466fb2d62fe36d9fe4a3cf827e1c278d4f0b
sal1hov <nhtxwd@gmail.com>
2025-01-26 04:02:36 +0300

    Обновление путей в aссounts

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import profile, profile_edit, delete_child, add_child
+from .views import profile, profile_edit, delete_child, add_child, change_password
 
 urlpatterns = [
     path('profile/', profile, name='profile'),
     path('profile/edit/', profile_edit, name='profile_edit'),
     path('profile/add_child/', add_child, name='add_child'),
     path('profile/delete_child/<int:child_id>/', delete_child, name='delete_child'),
+    path('change-password/', change_password, name='change_password'),  # Используем кастомное представление
 ]
