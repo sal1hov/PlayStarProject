4542d2af66e5163091b4868e6ebb52adf832128b
sal1hov <nhtxwd@gmail.com>
2025-01-31 09:57:38 +0300

    Обновление

--- Project/bookings/forms.py
+++ Project/bookings/forms.py
@@ -5,8 +5,9 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['event_name', 'event_date']
+        fields = ['event_name', 'event_date', 'status']
         widgets = {
             'event_name': forms.TextInput(attrs={'class': 'w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'}),
             'event_date': forms.DateTimeInput(attrs={'class': 'w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500', 'type': 'datetime-local'}),
+            'status': forms.Select(attrs={'class': 'w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'}),
         }
