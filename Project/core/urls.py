9449477524755830428fd3d45325a6e1affe0590
sal1hov <nhtxwd@gmail.com>
2025-04-28 03:19:37 +0300

    Обновление 28.04: вывод мероприятии для гостей

--- Project/core/urls.py
+++ Project/core/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from main.views import index, register, profile_view, logout_view
-from staff.views import edit_user, delete_user, manage_booking  # Исправили импорт
+from staff.views import edit_user, delete_user, manage_booking
 from bookings.views import edit_booking, delete_booking
 
 urlpatterns = [
@@ -13,9 +13,11 @@ urlpatterns = [
     path('accounts/logout/', logout_view, name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('bookings/', include('bookings.urls')),
-    path('staff/', include('staff.urls')),  # Подключаем маршруты для staff
+    path('staff/', include('staff.urls')),
     path('profile/', profile_view, name='profile'),
     path('booking/<int:booking_id>/edit/', edit_booking, name='edit_booking'),
     path('booking/<int:booking_id>/delete/', delete_booking, name='delete_booking'),
     path('booking/<int:booking_id>/<str:action>/', manage_booking, name='manage_booking'),
+    # Добавьте эту строку для включения URL-паттернов из main.urls
+    path('', include('main.urls')),
 ]
